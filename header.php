0e5d2927019bf83712db2b5e86f4578363278048
Lucas Beauloi <l.beauloi@gmail.com>
2023-11-17 13:31:04 +0100

    yesyes

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/scss/style.scss">
+    <link rel="stylesheet" href="\assets\css\style.css" media="screen">
     <title>Register</title>
 </head>
 <header>
